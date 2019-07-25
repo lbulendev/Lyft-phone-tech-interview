45a73737354a3884101f8035f9fd89f0b5c3ff5b
Larry Bulen <larry@nearchus.com>
2019-07-25 01:07:30 -0700

    more files for problem 1

--- StampPhoneScreen1/StampPhoneScreen1/ViewController.swift
+++ StampPhoneScreen1/StampPhoneScreen1/ViewController.swift
@@ -9,12 +9,33 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet var stampButtons: [UIButton]!
+    private var selectedColor: UIColor = .black
+    
+    @IBAction func stampTapped(_ sender: UIButton) {
+        guard let stampColor = sender.backgroundColor else {
+            return
+        }
+        
+        self.selectedColor = stampColor
+        self.updateButtonColors(selectedButton: sender)
     }
-
-
+    
+    private func updateButtonColors(selectedButton: UIButton) {
+        self.stampButtons.forEach { $0.alpha = 1.0 }
+        selectedButton.alpha = 0.5
+    }
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        print("touchesBegan")
+        for touch in touches {
+            print("x: \(touch.location(in: self.view).x) y: \(touch.location(in: self.view).y)")
+            let square: UIView = UIView.init()
+            square.frame.size.height = 50
+            square.frame.size.width = 50
+            square.center = CGPoint(x: touch.location(in: self.view).x, y: touch.location(in: self.view).y)
+            square.backgroundColor = selectedColor
+            self.view.addSubview(square)
+            
+        }
+    }
 }
-
